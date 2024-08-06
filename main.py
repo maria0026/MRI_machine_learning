5c0e285409d1cb1a92b51d9d7a31d8b753c112fe
maria0026 <m.waligorska@student.uw.edu.pl>
2024-08-06 14:05:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from scipy import stats
 import numpy as np
 import dimensions_reduction
 import prepare_dataset
+import plots
 
 '''
 path = 'data_4/original/Subjects.csv'
@@ -80,27 +81,9 @@ filename='data_4/cleaned_data/all_concatenated.csv'
 df=pd.read_csv(filename, sep='\t')
 print(df)
 
-#test normalnosci- bez kolumn typu płec 
-#columns_to_drop=['identifier', 'norm_confirmed', 'sex', 'male', 'female']
-#anomalies_detection.test_normality(filename, columns_to_drop)
+plots.plot_some_data(df)
 
-'''
-stat, p = shapiro(df['APARC-ctx-lh-bankssts_ThickAvg'])
-print(stat,p)
-plt.hist(df['age'])
-plt.show()
 
-sns.boxplot(x=df['age'])
-plt.show()
-z = np.abs(stats.zscore(df['age']))
-print(z)
-threshold_z = 2
- 
-outlier_indices = np.where(z > threshold_z)[0]
-no_outliers = df.drop(outlier_indices)
-print("Original DataFrame Shape:", df.shape)
-print("DataFrame Shape after Removing Outliers:", no_outliers.shape)
-'''
 
 #redukcja wymiarowości
 folder='data_4/cleaned_data'
@@ -115,7 +98,7 @@ folder=folder_out
 folder_out='data_4/correlation_matrices_pairs'
 #dimensions_reduction.calculate_correlation_matrices(folder, folder_out)
 
-
+'''
 #wyswietlenie korelacji
 df=pd.read_csv('data_4/correlation_matrices/BRAIN_correlation_matrix.csv', sep='\t', index_col=0)
 sns.heatmap(df, annot=True)
@@ -135,3 +118,4 @@ print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 #PCA
 dimensions_reduction.principal_component_analysis(X_train, X_test)
 
+'''
