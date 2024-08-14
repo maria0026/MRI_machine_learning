1b1d3f5a4ddfc1815ddcdd6be17432a73094a731
maria0026 <m.waligorska@student.uw.edu.pl>
2024-08-14 13:32:32 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import pandas as pd
 
 def random_forest_model(X_test, y_test, feature, rf):
     
@@ -17,4 +22,32 @@ def random_forest_model(X_test, y_test, feature, rf):
     # Create the confusion matrix
     cm = confusion_matrix(y_test[feature], y_pred)
 
-    return accuracy, precision, recall, cm
+    #roc curve
+    y_pred_proba = rf.predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_test[feature], y_pred_proba) 
+    
+
+    return accuracy, precision, recall, cm, fpr, tpr
+
+def random_forest_regression_model(X_test, y_test, feature, rf):
+
+    # Create a variable for the best model
+    best_rf = rf.best_estimator_
+
+    # Print the best hyperparameters
+    print('Best hyperparameters:',  rf.best_params_)
+
+    y_pred = rf.predict(X_test)
+    #export y_test and y_pred to df and then to csv
+    print(y_test[feature].shape, y_pred.shape)
+    # Make sure y_test[feature] and y_pred are aligned
+    results_df = pd.DataFrame({
+        'Actual': y_test[feature].values,  # Convert to NumPy array if it's not already
+        'Predicted': y_pred
+    })
+    results_df.to_csv('results.csv')
+    
+    mse=mean_squared_error(y_test[feature], y_pred)
+    rmse = float(format(np.sqrt(mean_squared_error(y_test[feature], y_pred)), '.3f'))
+
+    return mse, rmse
