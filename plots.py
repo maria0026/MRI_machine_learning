5a76874aa2522534e8cf388e4c1cc2199ea002b0
maria0026 <m.waligorska@student.uw.edu.pl>
2024-08-14 13:31:55 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -7,6 +7,9 @@ from sklearn.tree import export_graphviz
 from IPython.display import Image
 import graphviz
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import roc_curve, auc
+import plotly.express as px
+import plotly.io as pio
 
 def plot_some_data(df):
     #test normalnosci- bez kolumn typu płec 
@@ -39,27 +42,25 @@ def pca(pca_mri, train_pca, test_pca, X_train, y_train, X_test, y_test):
     test_principal_Df = pd.DataFrame(data = test_pca
              , columns = [str(i) for i in range(1,test_pca.shape[1]+1)], index=X_test.index)
     
-    #stosunek wyjasnionej wariancji
-    explained_variance_ratio=pca_mri.explained_variance_ratio_
 
     y_train['age_label']=y_train['age'].apply(lambda x: 1 if x>30 else 0)
     targets = [1,0]
     colors = ['r', 'g']
 
-    plt.figure(figsize=(10,20))
+    plt.figure(figsize=(20,10))
     plt.subplot(1, 2, 1)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=14)
     plt.xlabel('Principal Component 1',fontsize=20)
     plt.ylabel('Principal Component 2',fontsize=20)
-    plt.title("Train dataset",fontsize=20)
+    plt.title("Train dataset PCA",fontsize=20)
     for target, color in zip(targets,colors):
         indicesToKeep = y_train['male'] == target
         print(indicesToKeep)
         plt.scatter(train_principal_Df.loc[indicesToKeep, '1'], 
                     train_principal_Df.loc[indicesToKeep, '2'], 
                     c=color, s=50)
-    plt.legend(targets,prop={'size': 15})
+    plt.legend(['male', 'female'],prop={'size': 15})
 
 
     plt.subplot(1, 2, 2)
@@ -67,20 +68,21 @@ def pca(pca_mri, train_pca, test_pca, X_train, y_train, X_test, y_test):
     plt.yticks(fontsize=14)
     plt.xlabel('Principal Component 1',fontsize=20)
     plt.ylabel('Principal Component 2',fontsize=20)
-    plt.title("Test dataset",fontsize=20)
+    plt.title("Test dataset PCA",fontsize=20)
    
     for target, color in zip(targets,colors):
         indicesToKeep = y_test['male'] == target
         plt.scatter(test_principal_Df.loc[indicesToKeep, '1'], 
                     test_principal_Df.loc[indicesToKeep, '2'], 
-                    c=color, s=50)
-    plt.legend(targets,prop={'size': 15})
+                    c=color, s=50, cmap='viridis')
+    plt.legend(['male', 'female'],prop={'size': 15})
     plt.show()
 
+    
 def scree_plot(pca_mri):
 
     PC_values = np.arange(pca_mri.n_components_) + 1
-    plt.figure(figsize=(10,20))
+    plt.figure(figsize=(20,10))
     plt.subplot(1, 2, 1)
     plt.plot(PC_values, pca_mri.explained_variance_, 'o-', linewidth=2, color='blue')
     plt.title('Scree Plot')
@@ -90,9 +92,12 @@ def scree_plot(pca_mri):
     plt.subplot(1, 2, 2)
     plt.plot(PC_values, pca_mri.explained_variance_ratio_, 'o-', linewidth=2, color='blue')
     plt.title('Scree Plot')
-    plt.xlabel('Principal Component Ratio')
-    plt.ylabel('Variance Explained')
+    plt.xlabel('Principal Component')
+    plt.ylabel('Variance Explained Ratio')
     plt.show()
+
+
+
 
 def random_forest(X_train, rf):
 
@@ -125,9 +130,99 @@ def random_forest_features(X_train, rf):
 def correlation_matrix(correlations):
 
   
-    plt.figure(figsize=(15,10))
+    plt.figure(figsize=(20, 20))
     sns.heatmap(round(correlations,2), cmap='RdBu',  
             vmin=-1, vmax=1);
     plt.show()
+
+def roc_curve(fpr, tpr):
+    roc_auc = auc(fpr, tpr)
+
+    # Plot the ROC curve
+    plt.figure()  
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend()
+    plt.show()
     
     
+def t_sne(pca_mri, train_pca, X_train, y_train):
+    
+
+    train_principal_Df = pd.DataFrame(data = train_pca
+             , columns = [str(i) for i in range(1,train_pca.shape[1]+1)], index=X_train.index)
+
+    
+
+    y_train['age_label']=y_train['age'].apply(lambda x: 1 if x>30 else 0)
+    targets = [1,0]
+    colors = ['r', 'g']
+
+    plt.figure(figsize=(10,10))
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=14)
+    plt.xlabel('Principal Component 1',fontsize=20)
+    plt.ylabel('Principal Component 2',fontsize=20)
+    plt.title("Train dataset- t_SNE",fontsize=20)
+    for target, color in zip(targets,colors):
+        indicesToKeep = y_train['male'] == target
+        print(indicesToKeep)
+        plt.scatter(train_principal_Df.loc[indicesToKeep, '1'], 
+                    train_principal_Df.loc[indicesToKeep, '2'], 
+                    c=color, s=50)
+    plt.legend(['male', 'female'],prop={'size': 15})
+    plt.show()
+
+def roc_curve_crossvalid(tprs, fprs, aucs, feature):
+
+    mean_fpr=np.mean(fprs, axis=0)
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    print("srednie pole", mean_auc)
+    std_auc = np.std(aucs)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.plot(
+        mean_fpr,
+        mean_tpr,
+        color="b",
+        label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+        lw=2,
+        alpha=0.8,
+    )
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(
+        mean_fpr,
+        tprs_lower,
+        tprs_upper,
+        color="grey",
+        alpha=0.2,
+        label=r"$\pm$ 1 std. dev.",
+    )
+
+    ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title=f"Mean ROC curve with variability\n(Positive label '{feature}')",
+    )
+    ax.legend(loc="lower right")
+    plt.show()
+
+def correlations_total_volume(df):
+    sns.set(style="whitegrid")
+    fig, ax = plt.subplots(figsize=(15, 10))
+    df.set_index('Metric').plot(kind='bar', ax=ax)
+    ax.set_ylabel('Correlation with Intracranial Volume')
+    ax.set_title('Gender-based Correlation Differences')
+    ax.legend(title='Gender')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.show()
