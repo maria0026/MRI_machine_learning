408bd19b0a620c824c2d5ffb7f8bcfbadc318215
maria0026 <m.waligorska@student.uw.edu.pl>
2024-08-20 13:45:24 +0200

    feature importance

--- main_sex.py
+++ main_sex.py
@@ -24,19 +24,22 @@ label_names=['male']
 
 df=pd.read_csv('data_4/normal/all_concatenated.csv', sep='\t')
 df=df.drop(columns=['identifier', 'norm_confirmed', 'sex', 'female'])
+#drop columns containing norm and Std
+df=df.drop(columns=[col for col in df.columns if 'norm' in col or 'Std' in col])
 
 for i in range(n_crosval):
+    prepare_dataset.divide_by_total_volume(df)
     X_train, X_test, y_train, y_test=prepare_dataset.split_dataset(df, label_names)
     X_train, X_test=prepare_dataset.standarize_data(X_train, X_test)
 
     #PCA
     components_nr=40
-    pca_mri, train_pca, test_pca, component_loadings=dimensions_reduction.principal_component_analysis(X_train, X_test, components_nr)
+    pca_mri, train_pca, test_pca, importance_df=dimensions_reduction.principal_component_analysis(X_train, X_test, components_nr)
+    
     explained_variance_ratio=pca_mri.explained_variance_ratio_
     formatted_explained_variance = [f"{num:.10f}" for num in explained_variance_ratio]
     print('Explained variability per principal component: {}'.format(formatted_explained_variance))
     print(len(formatted_explained_variance))
-    print(component_loadings.shape)
     print(train_pca.shape, test_pca.shape)
     #plots
     #plots.pca(pca_mri, train_pca, test_pca, X_train, y_train, X_test, y_test)
@@ -49,16 +52,43 @@ for i in range(n_crosval):
     test_principal_Df = pd.DataFrame(data = test_pca
                 , columns = [str(i) for i in range(1,test_pca.shape[1]+1)], index=X_test.index)
 
-    train_principal_Df['age']=X_train['age']
-    test_principal_Df['age']=X_test['age']
+    #train_principal_Df['age']=X_train['age']
+    #test_principal_Df['age']=X_test['age']
 
 
     X_train=train_principal_Df
     X_test=test_principal_Df 
     feature='male'
 
+    clf=train.svm_classification_model(X_train, y_train)
+    accuracy, precision, recall, cm, fpr, tpr=test.svm_classification_model(X_test, y_test, clf)
+
+    '''
     rf=train.random_forest_model(X_train, y_train, feature)
+    best_rf = rf.best_estimator_
+    feature_importances = pd.Series(best_rf.feature_importances_, index=X_train.columns).sort_values(ascending=False)
+
+
+    
+    feature_importances.index = feature_importances.index.astype(int)
+    print(feature_importances)
+    #sort ascending by indexes
+    feature_importances=feature_importances.sort_index()
+    print(feature_importances.index)
+    print("wart", feature_importances.values)
+    importance_df['component_importance_on_tree']=feature_importances.values
+    #save to csv
+    if i==0:
+        importance_df.to_csv('importance_sex.csv', sep='\t', index=True)
+    else:
+        #concatenate
+        importance_df_old=pd.read_csv('importance_sex.csv', sep='\t', index_col=0)
+        importance_df=pd.concat([importance_df_old, importance_df], axis=1)
+        importance_df.to_csv('importance_sex.csv', sep='\t', index=True)
+
     accuracy, precision, recall, cm, fpr, tpr=test.random_forest_model(X_test, y_test, feature, rf)
+    '''
+
     accuracies.append(accuracy)
     precisions.append(precision)
     recalls.append(recall)
