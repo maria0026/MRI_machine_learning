bbdb2abe05e48f816add00cb9aca9b599e78865a
maria0026 <m.waligorska@student.uw.edu.pl>
2024-08-26 15:44:19 +0200

    Update main_delete_unnormal_features.py

--- main_delete_unnormal_features.py
+++ main_delete_unnormal_features.py
@@ -2,12 +2,14 @@ import anomalies_detection
 import prepare_dataset
 import pandas as pd
 
+
+filename='data_4/test_data_all/all_concatenated.csv'
+
 filename='data_4/cleaned_data/all_concatenated.csv'
 df=pd.read_csv(filename, sep='\t')
 print(df)
 #plots.plot_some_data(df)
 
-filename='data_4/cleaned_data/all_concatenated.csv'
 columns_to_drop=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume']
 
 #for sex identyfication
@@ -22,3 +24,38 @@ print("ilosc nienormalnych",row_sum)
 folder='data_4/cleaned_data'
 folder_out='data_4/normal'
 prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
+
+folder='data_4/test_data'
+folder_out='data_4/test_data_normal'
+prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
+
+
+#unnormal from test data
+filename='data_4/test_data_normal/all_concatenated.csv'
+df=pd.read_csv(filename, sep='\t')
+print(df)
+#plots.plot_some_data(df)
+
+columns_to_drop=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume']
+anomalies_detection.test_normality(filename, columns_to_drop)
+
+filename='outliers_values.csv'
+df_outliers=pd.read_csv(filename, sep='\t',index_col=0)
+row_sum = df_outliers.loc['wynik testu'].sum()
+print("ilosc nienormalnych",row_sum)
+
+#usunięcie nienormalnych danych
+folder='data_4/normal'
+folder_out='data_4/normal'
+prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
+
+folder='data_4/test_data_normal'
+folder_out='data_4/test_data_normal'
+prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
+
+
+'''
+folder='data_4/test_data_all'
+folder_out='data_4/all'
+prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
+'''
