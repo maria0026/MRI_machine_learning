a7d3f5cc980b5403fdbeda5e2a72c365a234c73d
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-02 11:53:01 +0200

    recurrent neural network data preparation

--- utils/nn_data.py
+++ utils/nn_data.py
@@ -14,3 +14,39 @@ class Data(Dataset):
    
     def __len__(self):
         return self.len
+    
+class DataRNN(Dataset):
+    def __init__(self, X, y, sequence_length=1):
+        # Reshape X to include the sequence dimension if necessary
+        if len(X.shape) == 2:  # Check if X is 2D and needs a third dimension
+            input_size = X.shape[1]
+            self.X = torch.from_numpy(X.values.astype(np.float32)).reshape(-1, sequence_length, input_size)
+        else:
+            self.X = torch.from_numpy(X.values.astype(np.float32))
+        
+        self.y = torch.from_numpy(y.values.astype(np.float32))
+        self.len = self.X.shape[0]
+       
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+   
+    def __len__(self):
+        return self.len
+
+class DataRNN_binary(Dataset):
+    def __init__(self, X, y, sequence_length=1):
+        # Reshape X to include the sequence dimension if necessary
+        if len(X.shape) == 2:  # Check if X is 2D and needs a third dimension
+            input_size = X.shape[1]
+            self.X = torch.from_numpy(X.values.astype(np.float32)).reshape(-1, sequence_length, input_size)
+        else:
+            self.X = torch.from_numpy(X.values.astype(np.float32))
+        
+        self.y = torch.from_numpy(y.values.astype(np.int64))
+        self.len = self.X.shape[0]
+       
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+   
+    def __len__(self):
+        return self.len
