866bc57c5ae3de45ba3e9ca7c765fe607c11542f
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-02 11:53:15 +0200

    recurrent neural network model

--- utils/nn_model.py
+++ utils/nn_model.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
 
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -15,7 +16,7 @@ class NeuralNetwork(nn.Module):
        
     def forward(self, x):
         x = torch.nn.functional.relu(self.layer_1(x))
-        x = torch.nn.functional.tanh(self.layer_2(x))
+        x = torch.nn.functional.relu(self.layer_2(x))
         x = self.dropout(x)
         x = torch.nn.functional.leaky_relu(self.layer_3(x))
         x = torch.nn.functional.sigmoid(self.output_layer(x))
@@ -23,3 +24,31 @@ class NeuralNetwork(nn.Module):
         return x
 
 
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(RNNModel, self).__init__()
+        
+        # Number of hidden dimensions
+        self.hidden_dim = hidden_dim
+        
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+        
+        # RNN
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
+        
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+    
+    def forward(self, x):
+
+        # Initialize hidden state with zeros
+        h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+            
+        # One time step
+        out, hn = self.rnn(x, h0)
+        out = self.fc(out[:, -1, :]) 
+        #out = torch.relu(out)
+        #sigmoid
+        out = torch.sigmoid(out)
+        return out
