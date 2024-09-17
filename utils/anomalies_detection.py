46725b9db4c04e15997470ec1919767d8e48d3b3
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-17 16:48:55 +0200

    cleaning

--- utils/anomalies_detection.py
+++ utils/anomalies_detection.py
@@ -1,54 +1,52 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import shapiro 
 from scipy import stats
 
 def test_normality(filename, columns_to_drop):
     df=pd.read_csv(filename, sep='\t')
-    df_test=df.drop(columns=columns_to_drop)
-    wynik=0
-    mad_values = {}
-    df_outliers=pd.DataFrame(columns=df_test.columns, index=df_test.index)
+    df=df.drop(columns=columns_to_drop)
 
-    for column in df_test.columns:
-        data=df_test[column]
+    #df for outliers marking
+    df_outliers=pd.DataFrame(columns=df.columns, index=df.index)
+    normality_scores = {}
 
-        #wykrycie outlierów
+    for column in df.columns:
+        data=df[column]
+        #norality test before outlier detection
+        _, p= stats.kstest(data, 'norm', args=(np.mean(data), np.std(data)))
+
+        #outliers detection
         mad=stats.median_abs_deviation(data)
         median = np.median(data)
         threshold_mad = 3
         
-        #test normalności
-        #stat, p = shapiro(data)
-        #stat, p=sm.stats.diagnostic.lilliefors(data)
-        stat, p= stats.kstest(data, 'norm', args=(np.mean(data), np.std(data)))
-
         upper_bound = median + threshold_mad * mad
         lower_bound = median - threshold_mad * mad
 
-        #Oznaczanie outlierów w DataFrame
+        #marking outliers in df_outliers
         df_outliers[column] = ((data > upper_bound) | 
                              (data < lower_bound)).astype(int)
         
-        #sum number of 1 in column
-        outliers_number=df_outliers[column].sum()
-        #wybranie nieoutlierów
+        #choose only data without outliers
         data=data[df_outliers[column]==0]
-
-        #stat, p_after = shapiro(data)
-        #stat, p_after=sm.stats.diagnostic.lilliefors(data)
-        stat, p_after= stats.kstest(data, 'norm', args=(np.mean(data), np.std(data)))
         
-        if p_after < 0.05:
-            wynik=1
-        else:
-            wynik=0
-        mad_values[column]=[median, mad, outliers_number, p, p_after, wynik]
+        _, p_after= stats.kstest(data, 'norm', args=(np.mean(data), np.std(data)))
 
-    df_mad = pd.DataFrame(mad_values)
-    df_mad.insert(0, 'name', ['median', 'mad', 'outliers_number', 'p', 'p_after', 'wynik testu'])
-    
-    return df_mad, df_outliers
+        test_result=0
+        if p_after < 0.05:
+            test_result=1
+        else:
+            test_result=0
+        
+        #sum number of outliers in column
+        outliers_number=df_outliers[column].sum()
+        normality_scores[column]=[median, mad, outliers_number, p, p_after, test_result]
+
+    df_normality_scores = pd.DataFrame(normality_scores)
+    df_normality_scores.insert(0, 'name', ['median', 'mad', 'outliers_number', 'p', 'p_after', 'wynik testu'])
+    df_normality_scores.set_index('name', inplace=True)
+
+    return df_normality_scores, df_outliers
 
     
  
