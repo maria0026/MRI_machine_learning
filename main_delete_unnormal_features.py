2f675ee285cffe61d06bfc76fd797c35914437cd
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-17 16:48:29 +0200

    add parser

--- main_delete_unnormal_features.py
+++ main_delete_unnormal_features.py
@@ -1,41 +1,38 @@
 from utils import anomalies_detection
 from utils import prepare_dataset
 import pandas as pd
+import argparse
 
+def main(args):
 
-norm_confimed=3
+    filename=f'data/{args.data_type}_norm_confirmed/all_concatenated.csv'
+    df=pd.read_csv(filename, sep='\t')
+    print(df)
+    #plots.plot_some_data(df)
 
-if norm_confimed==1:
-    type='positive'
-elif norm_confimed==0:
-    type='negative'
-else:
-    type='all'
+    #searching for unnormal columns
+    df_normality_scores, df_outliers = anomalies_detection.test_normality(filename, args.columns_to_drop)
+    df_normality_scores.to_csv(f'results/{args.data_type}_outliers_values.csv', sep='\t', index=True)
+    df_outliers.to_csv(f'results/{args.data_type}_outliers.csv', sep='\t', index=True)
+    print(df_normality_scores)
+    row_sum = df_normality_scores.loc['wynik testu'].sum()
+    print("ilosc nienormalnych",row_sum)
 
-filename=f'data/{type}_norm_confirmed/all_concatenated.csv'
+    #delete unnormal columns
+    folder=f'data/{args.data_type}_norm_confirmed'
+    folder_out=f'data/{args.data_type}_norm_confirmed_normal'
+    prepare_dataset.detele_unnormal_columns(folder, folder_out, df_normality_scores)
 
+    #we sometimes want to test our model on the different dataset
+    if args.test_data_type!="None":
+        folder=f'data/{args.test_data_type}_norm_confirmed'
+        folder_out=f'data/{args.test_data_type}_norm_confirmed_normal'
+        prepare_dataset.detele_unnormal_columns(folder, folder_out, df_normality_scores)
 
-df=pd.read_csv(filename, sep='\t')
-print(df)
-#plots.plot_some_data(df)
-
-columns_to_drop=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume']
-
-df_mad, df_outliers= anomalies_detection.test_normality(filename, columns_to_drop)
-df_mad.to_csv(f'results/{type}_outliers_values.csv', sep='\t', index=False)
-df_outliers.to_csv(f'results/{type}_outliers.csv', sep='\t', index=True)
-
-filename=f'results/{type}_outliers_values.csv'
-df_outliers=pd.read_csv(filename, sep='\t',index_col=0)
-row_sum = df_outliers.loc['wynik testu'].sum()
-print("ilosc nienormalnych",row_sum)
-
-#usunięcie nienormalnych danych
-folder=f'data/{type}_norm_confirmed'
-folder_out=f'data/{type}_norm_confirmed_normal'
-prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
-
-folder=f'data/negative_norm_confirmed'
-folder_out=f'data/negative_norm_confirmed_normal'
-prepare_dataset.detele_unnormal_columns(folder, folder_out, df_outliers)
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("parser for deleting unnormal features")
+    parser.add_argument("--data_type", nargs="?", default="positive", help="Type of dataset based on norm_confirmed: positive/negative/all", type=str)
+    parser.add_argument("--test_data_type", nargs="?", default="None", help="Type of test dataset based on norm_confirmed: positive/negative/all/None, choose None if you don't want to test on the different dataset", type=str)
+    parser.add_argument("--columns_to_drop", nargs="?", default=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume'], help="Columns to drop", type=list)
+    args = parser.parse_args()
+    main(args)
