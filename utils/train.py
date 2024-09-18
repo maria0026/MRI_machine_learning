1864cb841194ba670f6a4935bda11f1da858559d
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-18 16:06:27 +0200

    cleaning

--- utils/train.py
+++ utils/train.py
@@ -5,109 +5,65 @@ from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, r
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn import svm
 import torch
-import numpy as np
-from torch.utils.data import Dataset, DataLoader
-from torch import nn
-from torch import optim
 import matplotlib.pyplot as plt
-from utils import nn_data
-from utils import nn_model
+from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
-import shap
 
-def random_forest_model(X_train, y_train, feature):
+from utils import nn_data, nn_model
 
 
-    param_dist = {'n_estimators': randint(50,500),
-                'max_depth': randint(1,35),
-                'min_samples_leaf': randint(1,8),
-                'min_samples_split': [2,3,5,8,10,14,20]}
-
-    # Create a random forest classifier
+def random_forest_model(X_train, y_train, param_dist, feature):
     rf = RandomForestClassifier(criterion='gini', max_features='sqrt')
-
-    # Use random search to find the best hyperparameters
     rand_search = RandomizedSearchCV(rf, 
                                     param_distributions = param_dist, 
                                     n_iter=10, 
                                     cv=5)
-
-    # Fit the random search object to the data
+    
     rand_search.fit(X_train, y_train[feature])
 
     return rand_search
 
-def random_forrest_regression_model(X_train, y_train, feature):
-
-    param_dist = {'n_estimators': randint(50,500),
-                'max_depth': randint(1,35),
-                'min_samples_leaf': randint(1,8),
-                'min_samples_split': [2,3,5,8,10,14,20]}
-    
-     # Create a random forest regressor
+def random_forrest_regression_model(X_train, y_train, param_dist, feature):
     rf = RandomForestRegressor()
-
-    # Use random search to find the best hyperparameters
     rand_search = RandomizedSearchCV(rf, 
                                     param_distributions = param_dist, 
                                     n_iter=10, 
                                     cv=5)
 
-    # Fit the random search object to the data
     rand_search.fit(X_train, y_train[feature])
 
     return rand_search
 
-def svm_classification_model(X_train, y_train):
-
-    #Create a svm Classifier
+def svm_classification_model(X_train, y_train, param_grid):
     clf = svm.SVC(probability=True)
-
-    param_grid = {'C': randint(30,40),  
-              'gamma': uniform(0.0001,0.0005-0.0001), 
-              'kernel': ['rbf']}  
-  
     grid = RandomizedSearchCV(clf, param_distributions= param_grid, n_iter=10, cv=5) 
-    #Train the model using the training sets
     grid.fit(X_train, y_train.values.ravel())
 
     return grid
 
-def svm_regression_model(X_train, y_train):
-    
+def svm_regression_model(X_train, y_train, param_grid):
     clf = svm.SVR()
-
-    param_grid = {'C': randint(30,40),  
-            'gamma': uniform(0.0001,0.0005-0.0001), 
-            'kernel': ['rbf']}  
-
     grid = RandomizedSearchCV(clf, param_distributions= param_grid, n_iter=10, cv=5) 
-    #Train the model using the training sets
     grid.fit(X_train, y_train.values.ravel())
 
     return grid
 
 
-
-def layer_neural_network(X_train, y_train, input_dim, hidden_dim, output_dim, learning_rate, loss_fn, num_epochs, momentum=0, weight_decay=0):
-    batch_size = 64
-    #Instantiate training data
-    train_data = nn_data.Data(X_train, y_train)
-    train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+def feed_forward_neural_network(train_dataloader, input_dim, hidden_dim, output_dim, learning_rate, loss_fn, num_epochs, momentum=0, weight_decay=0):
 
     model = nn_model.NeuralNetwork(input_dim, hidden_dim, output_dim)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)    
     print(model)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)    
+
     loss_values = []
     model.train()
     for epoch in range(num_epochs):
         for X, y in train_dataloader:
-            # zero the parameter gradients
+
             optimizer.zero_grad()
-        
-            # forward + backward + optimize
             pred = model(X)
+
             y = y.view(-1, 1)
             loss = loss_fn(pred, y)
             loss_values.append(loss.item())
@@ -127,16 +83,13 @@ def layer_neural_network(X_train, y_train, input_dim, hidden_dim, output_dim, le
 
     return model
 
-def recurrent_neural_network(X_train, y_train, seq_dim, input_dim, hidden_dim, layer_dim, output_dim, learning_rate, loss_fn, num_epochs, weight_decay=0):
-    batch_size = 64
-    
-    train_data = nn_data.DataRNN(X_train, y_train, sequence_length= seq_dim)
-    train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+def recurrent_neural_network(train_dataloader, seq_dim, input_dim, hidden_dim, layer_dim, output_dim, learning_rate, loss_fn, num_epochs, weight_decay=0):
+
     model = nn_model.RNNModel(input_dim, hidden_dim, layer_dim, output_dim)
-    print(model)
-     
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
   
+    print(model)
+    
     loss_list = []
     iteration_list = []
     count = 0
@@ -146,18 +99,9 @@ def recurrent_neural_network(X_train, y_train, seq_dim, input_dim, hidden_dim, l
 
             train  = Variable(features.view(-1, seq_dim, input_dim))
             labels = Variable(labels)
-                
-            # Clear gradients
             optimizer.zero_grad()
-            
-            # Forward propagation
             outputs = model(train)
-            #print(outputs)
-            
-            # Calculate softmax and ross entropy loss
             loss = loss_fn(outputs, labels)
-            
-            # Calculating gradients
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             
@@ -170,6 +114,5 @@ def recurrent_neural_network(X_train, y_train, seq_dim, input_dim, hidden_dim, l
                 iteration_list.append(count)
                 if count % 500 == 0:
                     print('Iteration: {}  Loss: {} '.format(count, loss.item()))
-    #explainer = shap.DeepExplainer(model, X_train_explain)
-    #shap_values = explainer.shap_values(X_train_values)
+
     return model
