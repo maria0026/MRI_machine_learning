bb78c9e4ace525e72de8f3c880acfd23190c9f63
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-18 16:08:51 +0200

    classes

--- main_prepare.py
+++ main_prepare.py
@@ -3,11 +3,12 @@ import argparse
 import os
 
 def main(args):
-
-    subjects_path = os.path.join(args.path, 'Subjects.csv')
-    prepare_csv.replace_comma_with_dot(subjects_path)
-    prepare_csv.convert_delimiter(args.path, args.delimiter, args.old_delimiter, args.old_delimiter_2)
-    prepare_csv.convert_line_endings(args.path)
+    
+    processor = prepare_csv.FileProcessor(args.path)
+    
+    processor.replace_comma_with_dot(args.subjects_file)
+    processor.convert_delimiter(args.delimiter, args.old_delimiter, args.old_delimiter_2)
+    processor.convert_line_endings()
 
     if args.data_type=="positive":
         norm_confimed=1
@@ -20,33 +21,32 @@ def main(args):
     columns=['Brain_Segmentation_Volume','Brain_Segmentation_Volume_Without_Ventricles','Brain_Segmentation_Volume_Without_Ventricles_from_Surf','Total_cortical_gray_matter_volume','Supratentorial_volume','Supratentorial_volume.1', 'Estimated_Total_Intracranial_Volume','Brain Segmentation Volume',	'Brain Segmentation Volume Without Ventricles']
     #z wszystkich oprócz 1
     filenames=['LHA2009.csv', 'LHAPARC.csv', 'LHDKT.csv', 'RHA2009.csv', 'RHAPARC.csv','ASEG.csv', 'BRAIN.csv', 'WM.csv']
-    prepare_csv.delete_specified_columns(args.path, filenames, columns)
+    processor.delete_specified_columns(filenames, columns)
 
     columns=['White_Surface_Total_Area', 'Mean_Thickness']
     filenames=['LHA2009.csv', 'LHAPARC.csv', 'RHA2009.csv', 'RHAPARC.csv']
-    prepare_csv.delete_specified_columns(args.path, filenames, columns)
+    processor.delete_specified_columns(filenames, columns)
 
     columns=[ 'Left hemisphere cortical gray matter volume', 'Right hemisphere cortical gray matter volume','ASEG-Left hemisphere cerebral white matter volume','ASEG-Right hemisphere cerebral white matter volume', 'ASEG-Total cerebral white matter volume','ASEG-Subcortical gray matter volume', 'ASEG-Total gray matter volume', 'ASEG-Supratentorial volume', 'ASEG-Mask Volume', 'ASEG-Supratentorial volume_notvent']
     filenames=['ASEG.csv']
-    prepare_csv.delete_specified_columns(args.path, filenames, columns)
+    processor.delete_specified_columns(filenames, columns)
 
     columns=['Total_cerebral_white_matter_volume', 'Volume of ventricles and choroid plexus', 'Left_hemisphere_cerebral_white_matter_volume', 'Right_hemisphere_cerebral_white_matter_volume']
     filenames=['BRAIN.csv']
-    prepare_csv.delete_specified_columns(args.path, filenames, columns)
+    processor.delete_specified_columns(filenames, columns)
 
     #for those files where these columms are, we add hemisphere name
     columns=['White_Surface_Total_Area', 'Mean_Thickness']
     filenames=['LHDKT.csv', 'RHDKT.csv']
-    prepare_csv.add_hemisphere_name(args.path, filenames, columns)
+    processor.add_hemisphere_name(filenames, columns)
 
 
     #searching for indexes of subjects with missing data, duplicates and norm_confired subset
-    indexes=prepare_csv.get_indexes_for_cleaning_dataset(filepath=subjects_path, data_files=False, norm_confirmed=norm_confimed)
+    indexes=processor.get_indexes_for_cleaning_dataset(args.subjects_file, data_files=False, norm_confirmed=norm_confimed)
     filenames = ['WM.csv', 'ASEG.csv', 'BRAIN.csv', 'LHA2009.csv', 'LHAPARC.csv', 'LHDKT.csv', 'RHA2009.csv', 'RHAPARC.csv', 'RHDKT.csv']
 
     for filename in filenames:
-        filepath = os.path.join(args.path, filename)
-        indexes_df=prepare_csv.get_indexes_for_cleaning_dataset(filepath=filepath, data_files=True, norm_confirmed=norm_confimed)
+        indexes_df=processor.get_indexes_for_cleaning_dataset(filename, data_files=True, norm_confirmed=norm_confimed)
         if len(indexes_df)>0:
             indexes.append(indexes_df)
         else:
@@ -57,22 +57,26 @@ def main(args):
     print("ilosc", len(indexes))
 
     #clean the data based on indexes
-    folder_out=f'data/{args.data_type}_norm_confirmed'
-    prepare_csv.clean_datasets(indexes, args.path, folder_out)
+    folder_out=f'data/{args.data_type}_{args.folder_out_end}'
+    processor.clean_datasets(indexes, folder_out)
 
     folder_name=folder_out
-    prepare_csv.convert_line_endings(folder_name)
+    processor.convert_line_endings()
     #we get "all_contatenated.csv" file
-    prepare_csv.concatenate_datasets(folder_name)
+    concatenated_df=processor.concatenate_datasets()
+    concatenated_df.to_csv(f'{args.path}/{args.contatenated_filename}', sep='\t', index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("parser for deleting unnormal features")
     parser.add_argument("--data_type", nargs="?", default="positive", help="Type of dataset based on norm_confirmed: positive/negative/all", type=str)
     parser.add_argument("--test_data_type", nargs="?", default="None", help="Type of test dataset based on norm_confirmed: positive/negative/all/None, choose None if you don't want to test on the different dataset", type=str)
+    parser.add_argument("--subjects_file", nargs="?", default="Subjects.csv", help="Name of the file with subjects", type=str)
+    parser.add_argument("--contatenated_filename", nargs="?", default="all_concatenated.csv", help="Name of the file with subjects", type=str)
     parser.add_argument("--columns_to_drop", nargs="?", default=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume'], help="Columns to drop", type=list)
     parser.add_argument("--path", nargs="?", default="data/original", help="Path to the folder where the original files are", type=str)
-    parser.add_argument("old_delimiter", nargs="?", default=",", help="Old delimiter", type=str)
-    parser.add_argument("old_delimiter_2", nargs="?", default=";", help="Old delimiter", type=str)
-    parser.add_argument("delimiter", nargs="?", default="\t", help="New delimiter", type=str)
+    parser.add_argument("--old_delimiter", nargs="?", default=",", help="Old delimiter", type=str)
+    parser.add_argument("--old_delimiter_2", nargs="?", default=";", help="Old delimiter", type=str)
+    parser.add_argument("--delimiter", nargs="?", default="\t", help="New delimiter", type=str)
+    parser.add_argument("--folder_out_end", nargs="?", default="norm_confirmed", help="End of folder name", type=str)
     args = parser.parse_args()
     main(args)
