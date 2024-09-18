5f091650aa8957cd24030f84bc8731e0f75d1804
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-18 16:08:24 +0200

    parser arguments and classes

--- main_sex.py
+++ main_sex.py
@@ -3,43 +3,44 @@ import numpy as np
 from sklearn.metrics import roc_curve, auc
 from torch import nn
 import argparse
-
-from utils import dimensions_reduction, prepare_dataset, plots, train, test
+from scipy.stats import randint, uniform
+from utils import dimensions_reduction, prepare_dataset, plots, train, test, nn_data
 
 
 def main(args):
 
+    preprocessor = prepare_dataset.DatasetPreprocessor()
+    reductor = dimensions_reduction.DimensionsReductor()
+
     tprs, fprs, aucs, accuracies, precisions, recalls= [], [], [], [], [], []
     
-    df=pd.read_csv(f'data/{args.data_type}_norm_confirmed_normal/all_concatenated.csv', sep='\t')
-    df=df.drop(columns=args.columns_to_drop)
+    df = pd.read_csv(f'data/{args.data_type}_norm_confirmed_normal/all_concatenated.csv', sep='\t')
+    df = df.drop(columns=args.columns_to_drop)
 
-    df_test=pd.read_csv(f'data/{args.test_data_type}_norm_confirmed_normal/all_concatenated.csv', sep='\t')
-    df_test=df_test.drop(columns=args.columns_to_drop)
+    df_test = pd.read_csv(f'data/{args.test_data_type}_norm_confirmed_normal/all_concatenated.csv', sep='\t')
+    df_test = df_test.drop(columns=args.columns_to_drop)
 
     #drop columns containing norm and Std
     #df=df.drop(columns=[col for col in df.columns if 'norm' in col or 'Std' in col])
 
     for i in range(args.n_crosval):
         if args.division_by_total_volume:
-            df=prepare_dataset.divide_by_total_volume(df)
-            df_test=prepare_dataset.divide_by_total_volume(df_test)
+            df = preprocessor.divide_by_total_volume(df)
+            df_test = preprocessor.divide_by_total_volume(df_test)
 
-        X_train, X_test, y_train, y_test=prepare_dataset.split_dataset(df, args.label_names, args.test_size)
+        X_train, X_test, y_train, y_test = preprocessor.split_dataset(df, args.label_names, args.test_size)
         if args.different_test_dataset:
-            X_test=df_test.drop(columns=args.label_names)
-            y_test=df_test[args.label_names]
-        X_train, X_test=prepare_dataset.standarize_data(X_train, X_test)
+            X_test = df_test.drop(columns=args.label_names)
+            y_test = df_test[args.label_names]
+        X_train, X_test = preprocessor.standarize_data(X_train, X_test)
 
         #PCA
-        pca_mri, train_pca, test_pca, importance_df=dimensions_reduction.principal_component_analysis(X_train, X_test, args.components_nr, n_features=args.n_most_important_features)
-        explained_variance_ratio=pca_mri.explained_variance_ratio_
+        pca_mri, train_pca, test_pca, importance_df = reductor.principal_component_analysis(X_train, X_test, args.components_nr, n_features=args.n_most_important_features)
+        explained_variance_ratio = pca_mri.explained_variance_ratio_
         formatted_explained_variance = [f"{num:.10f}" for num in explained_variance_ratio]
         print('Explained variability per principal component: {}'.format(formatted_explained_variance))
-        print(len(formatted_explained_variance))
         print(train_pca.shape, test_pca.shape)
-        #plots.pca(pca_mri, train_pca, test_pca, X_train, y_train, X_test, y_test)
-        #plots.scree_plot(pca_mri)
+
         train_principal_Df = pd.DataFrame(data = train_pca
                     , columns = [str(i) for i in range(1,train_pca.shape[1]+1)], index=X_train.index)
 
@@ -47,59 +48,59 @@ def main(args):
                     , columns = [str(i) for i in range(1,test_pca.shape[1]+1)], index=X_test.index)
 
 
-        X_train=train_principal_Df
-        X_test=test_principal_Df 
-        feature=args.label_names
+        X_train = train_principal_Df
+        X_test = test_principal_Df 
+        feature = args.label_names
 
         input_dim = args.components_nr
         loss_fn = nn.BCELoss()
 
         if args.model_name=="forest":
-            rf=train.random_forest_model(X_train, y_train, *feature)
+            rf = train.random_forest_model(X_train, y_train, args.forest_param_dist, *feature)
             accuracy, precision, recall, cm, fpr, tpr=test.random_forest_model(X_test, y_test, feature, rf)
             best_rf = rf.best_estimator_
             feature_importances = pd.Series(best_rf.feature_importances_, index=X_train.columns).sort_values(ascending=False)
             feature_importances.index = feature_importances.index.astype(int)
-            print(feature_importances)
+            #print(feature_importances)
             #sort ascending by indexes
-            feature_importances=feature_importances.sort_index()
+            feature_importances = feature_importances.sort_index()
 
-            importance_df['component_names']=feature_importances.index
-            importance_df['comp_imp']=feature_importances.values
+            importance_df['component_names'] = feature_importances.index
+            importance_df['comp_imp'] = feature_importances.values
 
 
         elif args.model_name=="svm":
-            clf=train.svm_classification_model(X_train, y_train)
-            accuracy, precision, recall, cm, fpr, tpr=test.svm_classification_model(X_test, y_test, clf)
+            clf = train.svm_classification_model(X_train, y_train, args.svm_param_dist)
+            accuracy, precision, recall, cm, fpr, tpr = test.svm_classification_model(X_test, y_test, clf)
 
 
         elif args.model_name=="fnn":
-        
-            model=train.layer_neural_network(X_train, y_train, input_dim, args.fnn_hidden_dim, args.output_dim, args.fnn_learning_rate, loss_fn, args.num_epochs, args.fnn_momentum, args.fnn_weight_decay)
-            accuracy, precision, recall, FPR, cm, fpr, tpr, df_fi=test.neural_network_classification(X_test, y_test, model)
+            train_dataloader = nn_data.load_fnn_data(X_train, y_train, args.batch_size)
+            model = train.feed_forward_neural_network(train_dataloader, input_dim, args.fnn_hidden_dim, args.output_dim, args.fnn_learning_rate, loss_fn, args.num_epochs, args.fnn_momentum, args.fnn_weight_decay)
+            accuracy, precision, recall, FPR, cm, fpr, tpr, df_fi = test.neural_network_classification(X_test, y_test, args.batch_size, model)
     
 
         elif args.model_name=="rnn":
-
-            model=train.recurrent_neural_network(X_train, y_train, args.rnn_seq_dim, input_dim, args.rnn_hidden_dim, args.rnn_layer_dim, args.output_dim, args.rnn_learning_rate, loss_fn, args.num_epochs, args.rnn_weight_decay)
-            accuracy, precision, recall, FPR, cm, fpr, tpr=test.recurrent_neural_network_classification(X_test, y_test, args.rnn_seq_dim, input_dim, model)
+            train_dataloader = nn_data.load_rnn_data(X_train, y_train, args.batch_size)
+            model = train.recurrent_neural_network(train_dataloader, args.rnn_seq_dim, input_dim, args.rnn_hidden_dim, args.rnn_layer_dim, args.output_dim, args.rnn_learning_rate, loss_fn, args.num_epochs, args.rnn_weight_decay)
+            accuracy, precision, recall, FPR, cm, fpr, tpr = test.recurrent_neural_network_classification(X_test, y_test, args.batch_size, args.rnn_seq_dim, input_dim, model)
 
 
         if args.model_name!="fnn":
             if i==0:
                 importance_df.to_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t')
             else:
-                importance_df_old=pd.read_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t', index_col=0)
-                importance_df=pd.concat([importance_df_old, importance_df], axis=1)
+                importance_df_old = pd.read_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t', index_col=0)
+                importance_df = pd.concat([importance_df_old, importance_df], axis=1)
                 importance_df.to_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t', index=True)
 
         else:
             if i==0:
-                df_fi=pd.concat([df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
+                df_fi = pd.concat([df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
                 df_fi.to_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t', index=False)
             else:
-                importance_df_old=pd.read_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t')
-                importance_df=pd.concat([importance_df_old.reset_index(drop=True), df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
+                importance_df_old = pd.read_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t')
+                importance_df = pd.concat([importance_df_old.reset_index(drop=True), df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
                 importance_df.to_csv(f'{args.results_directory}/importance_sex_{args.model_name}.csv', sep='\t', index=False)
         
 
@@ -132,8 +133,16 @@ if __name__ == "__main__":
     parser.add_argument("--components_nr", nargs="?", default=35, help="Number of components for principal component analysis", type=int)
     parser.add_argument("--n_crosval", nargs="?", default=5, help="Number of crossvalidation", type=int)
     parser.add_argument("--num_epochs", nargs="?", default=100, help="Number of epochs", type=int)
+    parser.add_argument("--forest_param_dist", nargs="?", default= {'n_estimators': randint(50,500),
+                'max_depth': randint(1,35),
+                'min_samples_leaf': randint(1,8),
+                'min_samples_split': [2,3,5,8,10,14,20]}, help="Parameters for random forest", type=dict)
+    parser.add_argument("--svm_param_dist", nargs="?", default={'C': randint(30,40),  
+              'gamma': uniform(0.0001,0.0005-0.0001), 
+              'kernel': ['rbf']}, help="Range of parameters for support vector machine", type=dict)
+    parser.add_argument("--batch_size", nargs="?", default=64, help="Batch size", type=int)
     parser.add_argument("--fnn_momentum", nargs="?", default=0.3, help="Momentum for feed forward neural network", type=float)
-    parser.add_argument("--_fnn_weight_decay", nargs="?", default=0.08, help="Weight decay for feed forward neural network", type=float)
+    parser.add_argument("--fnn_weight_decay", nargs="?", default=0.08, help="Weight decay for feed forward neural network", type=float)
     parser.add_argument("--rnn_weight_decay", nargs="?", default=0.008, help="Weight decay for recurrent neural network", type=float)
     parser.add_argument("--fnn_learning_rate", nargs="?", default=0.075, help="Learning rate for feed forward neural network", type=float)
     parser.add_argument("--rnn_learning_rate", nargs="?", default=1e-3, help="Learning rate for recurrent neural network", type=float)
