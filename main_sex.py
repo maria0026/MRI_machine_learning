12be4f45bb20599e59a2c3c521ba3855144f1d49
maria0026 <m.waligorska@student.uw.edu.pl>
2024-09-26 11:46:51 +0200

    cleaning

--- main_sex.py
+++ main_sex.py
@@ -5,7 +5,7 @@ from torch import nn
 import argparse
 from scipy.stats import randint, uniform
 from utils import dimensions_reduction, prepare_dataset, plots, train, valid, test, nn_data
-
+import json
 
 def main(args):
 
@@ -33,7 +33,8 @@ def main(args):
         if args.different_test_dataset:
             X_test = df_test.drop(columns=args.label_names)
             y_test = df_test[args.label_names]
-        X_train, _,  X_test = preprocessor.standarize_data(X_train, X_test)
+
+        X_train, _,  X_test = preprocessor.standardize_data(X_train, X_val, X_test)
 
         #PCA
         pca_mri, train_pca, _, test_pca, importance_df = reductor.principal_component_analysis(X_train, X_test, args.components_nr, n_features=args.n_most_important_features)
@@ -57,6 +58,10 @@ def main(args):
         loss_fn = nn.BCELoss()
 
         if args.model_name=="forest":
+            forest_param_dist = json.loads(args.forest_param_dist)
+            forest_param_dist['n_estimators'] = randint(*forest_param_dist['n_estimators'])
+            forest_param_dist['max_depth'] = randint(*forest_param_dist['max_depth'])
+            forest_param_dist['min_samples_leaf'] = randint(*forest_param_dist['min_samples_leaf'])
             rf = trainer.random_forest_model(X_train, y_train, args.forest_param_dist, *feature)
             accuracy, precision, recall, cm, fpr, tpr=test.random_forest_model(X_test, y_test, feature, rf)
             best_rf = rf.best_estimator_
@@ -71,39 +76,32 @@ def main(args):
 
 
         elif args.model_name=="svm":
-            clf = trainer.svm_classification_model(X_train, y_train, args.svm_param_dist)
+            svm_param_dist = json.loads(args.svm_param_dist)
+            svm_param_dist['C'] = randint(*svm_param_dist['C'])  
+            svm_param_dist['gamma'] = uniform(*svm_param_dist['gamma']) 
+            clf = trainer.svm_classification_model(X_train, y_train, svm_param_dist)
             accuracy, precision, recall, cm, fpr, tpr = test.svm_classification_model(X_test, y_test, clf)
 
 
         elif args.model_name=="fnn":
-            train_dataloader = nn_data.load_fnn_data(X_train, y_train, args.batch_size)
+            train_dataloader = nn_data.load_fnn_data(X_train, y_train, args.batch_size, feature)
             model = trainer.feed_forward_neural_network(train_dataloader, input_dim, args.fnn_hidden_dim, args.output_dim, args.fnn_learning_rate, loss_fn, args.num_epochs, args.fnn_momentum, args.fnn_weight_decay)
-            accuracy, precision, recall, FPR, cm, fpr, tpr, df_fi = test.neural_network_classification(X_test, y_test, args.batch_size, model)
-    
+            accuracy, precision, recall, FPR, cm, fpr, tpr, df_fi = test.neural_network_classification(X_test, y_test, args.batch_size, model, feature)
+            importance_df = pd.concat([df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
 
         elif args.model_name=="rnn":
-            train_dataloader = nn_data.load_rnn_data(X_train, y_train, args.batch_size)
+            train_dataloader = nn_data.load_rnn_data(X_train, y_train, args.batch_size, feature)
             model = trainer.recurrent_neural_network(train_dataloader, args.rnn_seq_dim, input_dim, args.rnn_hidden_dim, args.rnn_layer_dim, args.output_dim, args.rnn_learning_rate, loss_fn, args.num_epochs, args.rnn_weight_decay)
-            accuracy, precision, recall, FPR, cm, fpr, tpr = test.recurrent_neural_network_classification(X_test, y_test, args.batch_size, args.rnn_seq_dim, input_dim, model)
+            accuracy, precision, recall, FPR, cm, fpr, tpr = test.recurrent_neural_network_classification(X_test, y_test, args.batch_size, args.rnn_seq_dim, input_dim, model, feature)
 
 
-        if args.model_name!="fnn":
-            if i==0:
-                importance_df.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t')
-            else:
-                importance_df_old = pd.read_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index_col=0)
-                importance_df = pd.concat([importance_df_old, importance_df], axis=1)
-                importance_df.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index=True)
-
+        if i==0:
+            importance_df.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t')
         else:
-            if i==0:
-                df_fi = pd.concat([df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
-                df_fi.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index=False)
-            else:
-                importance_df_old = pd.read_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t')
-                importance_df = pd.concat([importance_df_old.reset_index(drop=True), df_fi.reset_index(drop=True), importance_df.reset_index(drop=True)], axis=1)
-                importance_df.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index=False)
-        
+            importance_df_old = pd.read_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index_col=0)
+            importance_df = pd.concat([importance_df_old, importance_df], axis=1)
+            importance_df.to_csv(f'{args.results_directory}/{args.data_type}_importance_sex_{args.model_name}.csv', sep='\t', index=True)
+      
 
         accuracies.append(accuracy)
         precisions.append(precision)
@@ -129,19 +127,23 @@ if __name__ == "__main__":
     parser.add_argument("--data_type", nargs="?", default="positive", help="Type of dataset based on norm_confirmed: positive/negative/all", type=str)
     parser.add_argument("--test_size", nargs="?", default=0.2, help="Size of test dataset", type=float)
     parser.add_argument("--test_data_type", nargs="?", default="positive", help="Type of test dataset based on norm_confirmed: positive/negative/all", type=str)
-    parser.add_argument("--validation", nargs="?", default=False, help="create validation set: True/False", type=bool)
+    parser.add_argument("--validation", nargs="?", default=0, help="create validation set: 0/1", type=int)
     parser.add_argument("--division_by_total_volume", nargs="?", default=False, help="Divide volumetric data by Estimated_Total_Intracranial_Volume", type=bool)
     parser.add_argument("--n_most_important_features", nargs="?", default=20, help="Choose the number of extracting features that load into components")
     parser.add_argument("--components_nr", nargs="?", default=35, help="Number of components for principal component analysis", type=int)
     parser.add_argument("--n_crosval", nargs="?", default=5, help="Number of crossvalidation", type=int)
     parser.add_argument("--num_epochs", nargs="?", default=100, help="Number of epochs", type=int)
-    parser.add_argument("--forest_param_dist", nargs="?", default= {'n_estimators': randint(50,500),
-                'max_depth': randint(1,35),
-                'min_samples_leaf': randint(1,8),
-                'min_samples_split': [2,3,5,8,10,14,20]}, help="Parameters for random forest", type=dict)
-    parser.add_argument("--svm_param_dist", nargs="?", default={'C': randint(30,40),  
-              'gamma': uniform(0.0001,0.0005-0.0001), 
-              'kernel': ['rbf']}, help="Range of parameters for support vector machine", type=dict)
+    parser.add_argument("--forest_param_dist", nargs="?", default=json.dumps({
+        'n_estimators': [50, 500],
+        'max_depth': [1, 35],
+        'min_samples_leaf': [1, 8],
+        'min_samples_split': [2, 3, 5, 8, 10, 14, 20]}),
+        help="JSON string for random forest parameter distribution")
+    parser.add_argument("--svm_param_dist", nargs="?", default=json.dumps({
+        'C': [30, 40], 
+        'gamma': [0.0001, 0.0005],  
+        'kernel': ['rbf']  
+    }), help="JSON string for SVM parameter distribution")
     parser.add_argument("--batch_size", nargs="?", default=64, help="Batch size", type=int)
     parser.add_argument("--fnn_momentum", nargs="?", default=0.3, help="Momentum for feed forward neural network", type=float)
     parser.add_argument("--fnn_weight_decay", nargs="?", default=0.08, help="Weight decay for feed forward neural network", type=float)
