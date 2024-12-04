be878b262974e1e1fabd94fe4638e2a05ed91d80
maria0026 <m.waligorska@student.uw.edu.pl>
2024-12-04 17:56:59 +0100

    folder name change

--- main_delete_unnormal_features.py
+++ main_delete_unnormal_features.py
@@ -8,10 +8,7 @@ def main(args):
     preprocessor = prepare_dataset.DatasetPreprocessor()
     anomalies_detector = anomalies_detection.AnomaliesDetector()
 
-    filename = f'data/{args.data_type}_{args.folder_in_end}/{args.filename}'
-    df = pd.read_csv(filename, sep='\t')
-    #print(df)
-    #plots.plot_some_data(df)
+    filename = f'data/{args.data_type}_norm_confirmed/{args.filename}'
 
     #searching for unnormal columns
     df_normality_scores, df_outliers = anomalies_detector.test_normality(filename, args.columns_to_drop)
@@ -22,14 +19,14 @@ def main(args):
     print("ilosc nienormalnych",row_sum)
 
     #delete unnormal columns
-    folder = f'data/{args.data_type}_{args.folder_in_end}'
-    folder_out = f'data/{args.data_type}_{args.folder_out_end}'
+    folder = f'data/{args.data_type}_norm_confirmed'
+    folder_out = f'data/{args.data_type}_norm_confirmed_normal'
     preprocessor.detele_unnormal_columns(folder, folder_out, df_normality_scores)
 
     #we sometimes want to test our model on the different dataset
     if args.test_data_type!="None":
-        folder = f'data/{args.test_data_type}_{args.folder_in_end}'
-        folder_out = f'data/{args.test_data_type}_{args.folder_out_end}'
+        folder = f'data/{args.test_data_type}_norm_confirmed'
+        folder_out = f'data/{args.test_data_type}_norm_confirmed_normal'
         preprocessor.detele_unnormal_columns(folder, folder_out, df_normality_scores)
 
 if __name__ == "__main__":
@@ -38,8 +35,6 @@ if __name__ == "__main__":
     parser.add_argument("--test_data_type", nargs="?", default="None", help="Type of test dataset based on norm_confirmed: positive/negative/all/None, choose None if you don't want to test on the different dataset", type=str)
     parser.add_argument("--columns_to_drop", nargs="?", default=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume'], help="Columns to drop", type=list)
     parser.add_argument("--results_directory", nargs="?", default="results", help="Directory for results", type=str)
-    parser.add_argument("--folder_in_end", nargs="?", default="norm_confirmed", help="End of folder in name", type=str)
-    parser.add_argument("--folder_out_end", nargs="?", default="norm_confirmed_normal", help="End of folder out name", type=str)
     parser.add_argument("--filename", nargs="?", default="all_concatenated.csv", help="Name of the file for processing", type=str)
     args = parser.parse_args()
     main(args)
