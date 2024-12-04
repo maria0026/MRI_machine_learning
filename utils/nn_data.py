77c370edb9b481e8d4a59631db3ccc6148d06735
maria0026 <m.waligorska@student.uw.edu.pl>
2024-12-04 17:58:04 +0100

    identifiers feature

--- utils/nn_data.py
+++ utils/nn_data.py
@@ -5,19 +5,20 @@ from torch.utils.data import Dataset
 
 
 class Data(Dataset):
-    def __init__(self, X, y):
+    def __init__(self, X, y, identifiers):
         self.X = torch.from_numpy(X.values.astype(np.float32))
         self.y = torch.from_numpy(y.values.astype(np.float32))
+        self.identifiers = identifiers
         self.len = self.X.shape[0]
        
     def __getitem__(self, index):
-        return self.X[index], self.y[index]
+        return self.X[index], self.y[index], self.identifiers[index]
    
     def __len__(self):
         return self.len
     
 class DataRNN(Dataset):
-    def __init__(self, X, y, sequence_length):
+    def __init__(self, X, y, identifiers, sequence_length):
         # Reshape X to include the sequence dimension if necessary
         if len(X.shape) == 2:  # Check if X is 2D and needs a third dimension
             input_size = X.shape[1]
@@ -25,23 +26,24 @@ class DataRNN(Dataset):
         else:
             self.X = torch.from_numpy(X.values.astype(np.float32))
         
+        self.identifiers = identifiers
         self.y = torch.from_numpy(y.values.astype(np.float32))
         self.len = self.X.shape[0]
        
     def __getitem__(self, index):
-        return self.X[index], self.y[index]
+        return self.X[index], self.y[index], self.identifiers[index]
    
     def __len__(self):
         return self.len
 
 def load_fnn_data(X, y, batch_size, feature):
-    train_data = Data(X, y[feature])
+    train_data = Data(X, y[feature], y[feature].values)
     train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
     return train_dataloader
 
 def load_rnn_data(X, y, batch_size, feature):
-    train_data = DataRNN(X, y[feature], sequence_length=1)
+    train_data = DataRNN(X, y[feature], y[feature].values, sequence_length=1)
     train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
     return train_dataloader
