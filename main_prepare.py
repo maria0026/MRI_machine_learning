cdb5c60c097bfa075c1a60498d47595cde651993
maria0026 <m.waligorska@student.uw.edu.pl>
2024-12-04 17:57:24 +0100

    creating folders

--- main_prepare.py
+++ main_prepare.py
@@ -1,6 +1,8 @@
 import argparse
 from utils import prepare_csv
-
+import pandas as pd
+import os
+import shutil
 
 def main(args):
     
@@ -27,7 +29,7 @@ def main(args):
     filenames=['LHA2009.csv', 'LHAPARC.csv', 'RHA2009.csv', 'RHAPARC.csv']
     processor.delete_specified_columns(filenames, columns)
 
-    columns=[ 'Left hemisphere cortical gray matter volume', 'Right hemisphere cortical gray matter volume','ASEG-Left hemisphere cerebral white matter volume','ASEG-Right hemisphere cerebral white matter volume', 'ASEG-Total cerebral white matter volume','ASEG-Subcortical gray matter volume', 'ASEG-Total gray matter volume', 'ASEG-Supratentorial volume', 'ASEG-Mask Volume', 'ASEG-Supratentorial volume_notvent', 'Supratentorial volume.1']
+    columns=['Left hemisphere cortical gray matter volume', 'Right hemisphere cortical gray matter volume','ASEG-Left hemisphere cerebral white matter volume','ASEG-Right hemisphere cerebral white matter volume', 'ASEG-Total cerebral white matter volume','ASEG-Subcortical gray matter volume', 'ASEG-Total gray matter volume', 'ASEG-Supratentorial volume', 'ASEG-Mask Volume', 'ASEG-Supratentorial volume_notvent', 'Supratentorial volume.1']
     filenames=['ASEG.csv']
     processor.delete_specified_columns(filenames, columns)
 
@@ -47,36 +49,40 @@ def main(args):
 
     for filename in filenames:
         indexes_df=processor.get_indexes_for_cleaning_dataset(filename, data_files=True, norm_confirmed=norm_confimed)
-        if len(indexes_df)>0:
-            indexes.append(indexes_df)
-        else:
-            print(f'No missing data in {filename}')
+        if os.path.isfile(filename):
+            print("rozmiar", pd.read_csv(f'{args.path}/{filename}', sep='\t').shape)
+            if len(indexes_df)>0:
+                indexes.append(indexes_df)
+            else:
+                print(f'No missing data in {filename}')
 
     #delete duplicates from indexes
     indexes = list(set(indexes))
     print("ilosc", len(indexes))
 
     #clean the data based on indexes
-    folder_out=f'data/{args.data_type}_{args.folder_out_end}'
+    folder_out=f'data/{args.data_type}_norm_confirmed'
+    if os.path.exists(folder_out):
+        shutil.rmtree(folder_out)
+        os.makedirs(folder_out)
     processor.clean_datasets(indexes, folder_out)
 
     folder_name=folder_out
     processor.convert_line_endings()
-    #we get "all_contatenated.csv" file
     concatenated_df=processor.concatenate_datasets(folder_name)
-    print("concatnedated_df", concatenated_df.shape)
-    concatenated_df.to_csv(f'{folder_name}/{args.contatenated_filename}', sep='\t', index=False)
+    print("concatenated_df", concatenated_df.shape, "to", f'{folder_name}/all_concatenated.csv')
+    concatenated_df.to_csv(f'{folder_name}/all_concatenated.csv', sep='\t', index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("parser for deleting unnormal features")
     parser.add_argument("--data_type", nargs="?", default="positive", help="Type of dataset based on norm_confirmed: positive/negative/all", type=str)
     parser.add_argument("--subjects_file", nargs="?", default="Subjects.csv", help="Name of the file with subjects", type=str)
-    parser.add_argument("--contatenated_filename", nargs="?", default="all_concatenated.csv", help="Name of the file with subjects", type=str)
     parser.add_argument("--columns_to_drop", nargs="?", default=['identifier', 'norm_confirmed', 'sex', 'male', 'female', 'age','Estimated_Total_Intracranial_Volume'], help="Columns to drop", type=list)
     parser.add_argument("--path", nargs="?", default="data/original", help="Path to the folder where the original files are", type=str)
     parser.add_argument("--old_delimiter", nargs="?", default=",", help="Old delimiter", type=str)
     parser.add_argument("--old_delimiter_2", nargs="?", default=";", help="Old delimiter", type=str)
     parser.add_argument("--delimiter", nargs="?", default="\t", help="New delimiter", type=str)
-    parser.add_argument("--folder_out_end", nargs="?", default="norm_confirmed", help="End of folder out name", type=str)
+    #parser.add_argument("--contatenated_filename", nargs="?", default="all_concatenated.csv", help="Name of the file with subjects", type=str)
+    #parser.add_argument("--folder_out_end", nargs="?", default="norm_confirmed", help="End of folder out name", type=str)
     args = parser.parse_args()
     main(args)
