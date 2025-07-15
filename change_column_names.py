2a59c81aba80c306df078694eb1822c31df19b04
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-07-15 16:35:25 +0200

    Update change_column_names.py

--- change_column_names.py
+++ change_column_names.py
@@ -7,7 +7,7 @@ def main(args):
 
 if __name__ == "__main__":  
 
-    parser = argparse.ArgumentParser("parser for changing column names")  
+    parser = argparse.ArgumentParser("Parser for changing column names")  
     parser.add_argument("--path", nargs="?", default="data/pathology", help="Path to the folder where the original files are", type=str)
     args = parser.parse_args()
     main(args)
