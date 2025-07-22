d0d4d4c34d03b1f5ff12453df6a60e831f6eed0e
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-07-22 16:03:23 +0200

    nn model architecture

--- utils/nn_model.py
+++ utils/nn_model.py
@@ -8,20 +8,23 @@ import pandas as pd
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(NeuralNetwork, self).__init__()
-        self.layer_1 = nn.Linear(input_dim, hidden_dim*2)
+        self.layer_1 = nn.Linear(input_dim, hidden_dim*3)
         nn.init.kaiming_uniform_(self.layer_1.weight, nonlinearity="relu")
-        self.layer_2 = nn.Linear(hidden_dim*2, hidden_dim)
+        self.layer_2 = nn.Linear(hidden_dim*3, hidden_dim)
         nn.init.kaiming_uniform_(self.layer_2.weight, nonlinearity="relu")
         self.dropout = nn.Dropout(0.4)
         self.layer_3 = nn.Linear(hidden_dim, hidden_dim//2)
         nn.init.kaiming_uniform_(self.layer_3.weight, nonlinearity="relu")
-        self.output_layer = nn.Linear(hidden_dim//2, output_dim)
+        self.layer_4 = nn.Linear(hidden_dim//2, hidden_dim//4)
+        nn.init.kaiming_uniform_(self.layer_4.weight, nonlinearity="relu")
+        self.output_layer = nn.Linear(hidden_dim//4, output_dim)
        
     def forward(self, x):
         x = torch.nn.functional.relu(self.layer_1(x))
         x = torch.nn.functional.relu(self.layer_2(x))
         x = self.dropout(x)
-        x = torch.nn.functional.leaky_relu(self.layer_3(x))
+        x = torch.nn.functional.relu(self.layer_3(x))
+        x = torch.nn.functional.relu(self.layer_4(x))
         x = torch.nn.functional.sigmoid(self.output_layer(x))
 
         return x
